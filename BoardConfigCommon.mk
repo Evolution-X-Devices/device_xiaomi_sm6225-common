Nicooo-Afk/android_device_xiaomi_sm6225-common

sapphire: Include MiuiCamera if exists

BoardConfigCommon.mk
@@ -14,6 +14,9 @@ BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 BUILD_BROKEN_INCORRECT_PARTITION_IMAGES := true
 RELAX_USES_LIBRARY_CHECK := true
 
+# Inherit from proprietary files for miuicamera
+-include device/xiaomi/miuicamera-topaz/BoardConfig.mk
+
 # A/B
 AB_OTA_UPDATER := true
 

Evolution-X-Devices/device_xiaomi_sm6225-common

BoardConfigCommon.mk
@@ -14,6 +14,9 @@ BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 BUILD_BROKEN_INCORRECT_PARTITION_IMAGES := true
 RELAX_USES_LIBRARY_CHECK := true
 
+# Inherit from proprietary files for miuicamera
+-include device/xiaomi/miuicamera-sapphire/BoardConfig.mk
+
 # A/B
 AB_OTA_UPDATER := true
 