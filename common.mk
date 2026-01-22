c664edea52426b4b4d96ae8264dbcd7163b995fa
Bruno Martins <bgcngm@gmail.com>
2026-01-22 11:35:47 +0800

    sm6225-common: Work around using prebuilt audio primary HAL

--- common.mk
+++ common.mk
@@ -71,6 +71,7 @@ PRODUCT_PACKAGES += \
     libhfp_pal
 
 AUDIO_HAL_DIR := hardware/qcom-caf/sm6225/audio/primary-hal
+TARGET_PROVIDES_AUDIO_HAL := true
 
 PRODUCT_COPY_FILES += \
     $(AUDIO_HAL_DIR)/configs/common/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
